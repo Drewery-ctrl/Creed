fb8ffce38f6156025012d21e9c1007b0416d0e1d
Drewery-ctrl <hachi_nnodi@yahoo.com>
2021-01-20 14:53:42 -0600

    Added edited files pipe, fonts

---
+++ src/app/pipes/sort-by.pipe.ts
@@ -0,0 +1,25 @@
+/*
+ *Custom Sort Pipe
+ *ngFor="let c of oneDimensionalArray | sortBy:'asc'"
+ *ngFor="let c of arrayOfObjects | sortBy:'asc|desc':'propertyName'"
+*/
+import { Pipe, PipeTransform } from '@angular/core';
+import { orderBy } from 'lodash';
+
+@Pipe({
+  name: 'sortBy'
+})
+export class SortByPipe implements PipeTransform {
+
+  transform(value: any[], order = '', column: string = ''): any[] {
+    if (!value || order === '' || !order) { return value; } // no array
+    if (value.length <= 1) { return value; } // array with only one item
+    if (!column || column === '') {
+      if (order === 'asc'){return value.sort(); }
+      else{return value.sort().reverse(); }
+    } // sort 1d array
+    // @ts-ignore
+    return orderBy(value, [column], [order]);
+  }
+
+}
